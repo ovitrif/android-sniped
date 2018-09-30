af233996d6c578439cb5698b5b04a7b126c0f447
Ovi Trif <ovi.artist@gmail.com>
2018-09-30 12:44:11 +0200

    Remove unused method setDetailBtnBgColor

--- app/src/main/java/com/sniped/ui/main/IMain.kt
+++ app/src/main/java/com/sniped/ui/main/IMain.kt
@@ -1,12 +1,10 @@
 package com.sniped.ui.main
 
-import android.support.annotation.ColorInt
 import com.sniped.ui.Presentable
 
 interface IMain {
 
     interface View {
-        fun setDetailBtnBgColor(@ColorInt color: Int)
     }
 
     interface Presenter : Presentable {
