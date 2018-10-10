6f0778e68a6451deafa208f2e6a23b83df5a74f4
Ovi Trif <ovi.artist@gmail.com>
2018-10-10 21:58:27 +0200

    Add test app

---
+++ app/src/test/kotlin/com/sniped/test/TestApp.kt
@@ -0,0 +1,14 @@
+package com.sniped.test
+
+import com.sniped.App
+
+class TestApp : App() {
+
+    override fun installMultiDex() {
+        // don't install multidex
+    }
+
+    override fun init() {
+        // don't initialize
+    }
+}
