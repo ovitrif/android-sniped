a325cfa5badb295b65322582465eefde28201127
Ovi Trif <ovi.artist@gmail.com>
2018-10-13 22:46:12 +0200

    Introduce base classes for groupie library

---
+++ app/src/main/java/com/sniped/ui/adapter/BaseAdapter.kt
@@ -0,0 +1,10 @@
+package com.sniped.ui.adapter
+
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+
+/**
+ * Base adapter for recycler views.
+ * Uses the Groupie library to eliminate the need of writing ViewHolder classes.
+ */
+abstract class BaseAdapter : GroupAdapter<ViewHolder>()
---
+++ app/src/main/java/com/sniped/ui/adapter/BaseAdapterItem.kt
@@ -0,0 +1,10 @@
+package com.sniped.ui.adapter
+
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+
+/**
+ * Base item for recycler views.
+ * Uses the Groupie library to abstract the boilerplate of multiple item view types.
+ */
+abstract class BaseAdapterItem : Item<ViewHolder>()
