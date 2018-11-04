89f497a3f1b030c639828a708c1c1d419f8a5c81
Ovi Trif <ovi.artist@gmail.com>
2018-11-04 03:17:15 +0100

    Add jacoco code coverage reports

---
+++ app/src/test/java/com/sniped/ui/BasePresenterTest.kt
@@ -0,0 +1,16 @@
+package com.sniped.ui
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BasePresenterTest {
+
+    @Test
+    fun getDisposeBag() {
+    }
+
+    @Test
+    fun onHide() {
+    }
+}
